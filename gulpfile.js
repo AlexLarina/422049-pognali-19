b9fb7750b6dec0cb57acbe026ccffb65bbc07175
Alexandra Larina <aa.larina.ff@gmail.com>
2020-05-26 18:32:04 +0300

    Fixing D23: fonts preload

--- gulpfile.js
+++ gulpfile.js
@@ -84,7 +84,8 @@ gulp.task("html", function () {
 
 gulp.task("copy", function () {
   return gulp.src([
-    "source/fonts/**/*.{woff, woff2}",
+    "source/fonts/**/*.woff2",
+    "source/fonts/**/*.woff",
     "source/img/**",
     "source/js/**",
     "source/*.ico"
